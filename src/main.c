ffbd5013cf3e0e1883d468685ae2ee4b9a1560a7
root <mathieu.the@insa-rennes.fr>
2017-12-13 17:38:05 +0000

    1er Test

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main(int argc, const char * argv[]){
 	CURL *curl = curl_easy_init();
 	if(curl) {
   		CURLcode res;
-  		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+  		curl_easy_setopt(curl, CURLOPT_HTTPGET, "unix:///var/run/docker.sock/containers/json");
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 	}
