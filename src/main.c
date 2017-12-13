59f7865dc8b3a895c1265233370611cf79e3538b
root <mathieu.the@insa-rennes.fr>
2017-12-13 17:30:14 +0000

    Ajout MakeFile

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "cLibDocker.h"
 
 int main(int argc, const char * argv[]){
-	printf("Hello Word\n");
+	CURL *curl = curl_easy_init();
+	if(curl) {
+  		CURLcode res;
+  		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
 	return 0;
 }
